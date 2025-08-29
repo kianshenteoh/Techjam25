06aa0ae11d26c3de25f62d5edcca0601feb63c25
Choo Kai Xi Kasey <e1366464@u.nus.edu>
2025-08-30 02:12:50 +0800

    Add HomeGpt.tsx, HomeGpt.css, HomeClaude.tsx, HomeClaude.css

--- Thunderbolt/src/index.tsx
+++ Thunderbolt/src/index.tsx
@@ -4,8 +4,9 @@ import { root } from '@lynx-js/react'
 
 import { App } from './App.tsx'
 import { Query } from './Query.tsx'
+import { HomeGpt } from './HomeGpt.tsx'
 
-root.render(<Query />)
+root.render(<HomeGpt />)
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
